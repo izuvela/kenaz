09334fadfc3f943ab6662a7ff03a3cc7a11c9d8d
Ivan Žuvela <ivan.zuvela@factory.dev>
2023-07-31 14:36:06 +0200

    Featured posts

---
+++ src/components/Footer/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post">
+    <div class="post__wrapper">
+      <div class="post__text">
+        <div class="post__subtext">
+          <div class="post__date">August 26, 2013</div>
+          <div class="post__number">5</div>
+        </div>
+        <div class="post__title">Palestinians call off peace talks after clash</div>
+      </div>
+      <img class="post__image" src="../../assets/woman-sun.png"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Divider from "./Divider.vue";
+export default {
+  components: {
+    Divider,
+  },
+};
+</script>
--- src/components/Footer/Posts.vue
+++ src/components/Footer/Posts.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="posts">
-    <Title color="white" :text="titleText" />
+    <Title color="white" :text="titleText" class="posts__title" />
+    <Post />
+    <Divider class="posts__divider" />
+    <Post />
+    <Divider class="posts__divider" />
+    <Post />
   </div>
 </template>
 
 <script>
 import Title from "../General/Title.vue";
+import Post from "./Post.vue";
+import Divider from "./Divider.vue";
 
 export default {
-  components: { Title },
+  components: { Title, Post, Divider },
   props: {
     titleText: {
       required: true,

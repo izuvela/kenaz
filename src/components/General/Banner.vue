fa2050a9f4048d8e26aedaebfde5c92332f48aef
Ivan Žuvela <ivan.zuvela@factory.dev>
2023-08-01 12:57:22 +0200

    Banner

---
+++ src/components/General/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="banner" :style="{ width: `${width}px`, height: `${height}px` }">
+    <div class="banner__inner">
+      <div class="banner__title">banner</div>
+      <div class="banner__size">{{ width }}x{{ height }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>HOME</div>
-  <router-link :to="{ name: 'Single' }">Single</router-link>
-  <router-link :to="{ name: 'Category' }">Category</router-link>
+  <!-- <router-link :to="{ name: 'Single' }">Single</router-link>
+  <router-link :to="{ name: 'Category' }">Category</router-link> -->
+  <div class="content">
+    <div class="content__wrapper">
+      <Banner width="940" height="120" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Banner from "../components/General/Banner.vue";
+
+export default {
+  components: {
+    Banner,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>

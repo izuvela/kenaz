6f42390a9da6e4ab75adecff768c8edf0e5512bf
Ivan Žuvela <ivan.zuvela@factory.dev>
2023-08-21 11:58:57 +0200

    Article content

---
+++ src/components/SingleArticle/SingleArticleContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="singleArticleContent">
+    <div class="singleArticleContent__text">{{ firstParagraph }}</div>
+    <img
+      src="../../assets/article-image-content.png"
+      alt="article image"
+      class="singleArticleContent__image"
+    />
+    <div class="singleArticleContent__text">{{ secondParagraph }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    firstParagraph: {
+      type: String,
+      required: true,
+    },
+    secondParagraph: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/screens/SingleScreen.vue
+++ src/screens/SingleScreen.vue
@@ -8,6 +8,10 @@
       />
       <div class="content__group">
         <div class="content__left">
+          <SingleArticleContent
+            firstParagraph="Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.Sed posuere consectetur est at lobortis. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo."
+            secondParagraph="Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem. Ac magna lobortis non, vulputate vitae viverra. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue a. Rhoncus maecenas, sit suspendisse, condimentum purus convallis dui hendrerit. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue. "
+          />
           <Banner width="620" height="120" />
         </div>
         <div class="content__right">
@@ -26,7 +30,7 @@ import Sidebar from "../components/General/Sidebar.vue";
 import Slider from "../components/Sliders/Slider.vue";
 import ArticleList from "../components/Articles/ArticleList.vue";
 import SingleArticleImage from "../components/SingleArticle/SingleArticleImage.vue";
-import ArticleImage from "../assets/article-image.png";
+import SingleArticleContent from "../components/SingleArticle/SingleArticleContent.vue";
 
 export default {
   components: {
@@ -36,6 +40,7 @@ export default {
     SideBanner,
     ArticleList,
     SingleArticleImage,
+    SingleArticleContent,
   },
 
   data() {

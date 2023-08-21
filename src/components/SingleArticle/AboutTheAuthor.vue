045d03c0fba2d6ae75eca3d7ea66c98feb73edcb
Ivan Žuvela <ivan.zuvela@factory.dev>
2023-08-21 12:15:34 +0200

    About the author component

---
+++ src/components/SingleArticle/AboutTheAuthor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aboutTheAuthor">
+    <Title text="About the Author" color="titleGrey" />
+    <div class="aboutTheAuthor__content">
+      <img
+        src="../../assets/author-image.png"
+        alt="author image"
+        class="aboutTheAuthor__image"
+      />
+      <div class="aboutTheAuthor__text">
+        Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec
+        hac ultrices nulla non netus. Placerat vehicula donec non suscipit
+        egestas, augue vel suspendisse. Et felis venenatis blandit sed est
+        ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien,
+        eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "../General/Title.vue";
+
+export default { components: { Title } };
+</script>
--- src/screens/SingleScreen.vue
+++ src/screens/SingleScreen.vue
@@ -12,6 +12,7 @@
             firstParagraph="Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.Sed posuere consectetur est at lobortis. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo."
             secondParagraph="Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna, at aliquam nullam facilisis aliquet sapien, eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem. Ac magna lobortis non, vulputate vitae viverra. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue a. Rhoncus maecenas, sit suspendisse, condimentum purus convallis dui hendrerit. Purus ipsum neque ipsum odio nulla, mi turpis diam tellus laoreet congue. "
           />
+          <AboutTheAuthor />
           <Banner width="620" height="120" />
         </div>
         <div class="content__right">
@@ -31,6 +32,7 @@ import Slider from "../components/Sliders/Slider.vue";
 import ArticleList from "../components/Articles/ArticleList.vue";
 import SingleArticleImage from "../components/SingleArticle/SingleArticleImage.vue";
 import SingleArticleContent from "../components/SingleArticle/SingleArticleContent.vue";
+import AboutTheAuthor from "../components/SingleArticle/AboutTheAuthor.vue";
 
 export default {
   components: {
@@ -41,6 +43,7 @@ export default {
     ArticleList,
     SingleArticleImage,
     SingleArticleContent,
+    AboutTheAuthor,
   },
 
   data() {

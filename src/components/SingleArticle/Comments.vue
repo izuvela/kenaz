b40d53aa606c19591cff768a2be50d75653705d9
Ivan Žuvela <ivan.zuvela@factory.dev>
2023-08-22 16:22:55 +0200

    Comment form style

--- src/components/SingleArticle/Comments.vue
+++ src/components/SingleArticle/Comments.vue
@@ -15,6 +15,40 @@
         text="Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec hac ultrices nulla non netus. Placerat vehicula donec non suscipit egestas, augue vel suspendisse. Et felis venenatis blandit sed est ultrices, adipiscing urna."
       />
     </div>
+    <Title
+      text="Add Your Comment"
+      color="titleGrey"
+      class="comments__title comments__title--secondary"
+    />
+    <div class="comments__text">
+      Molestias ultricies, ante quam urna ut volutpat, egestas dolor dui, nec
+      hac ultrices nulla non netus. Placerat vehicula donec non suscipit
+      egestas, augue vel suspendisse. Et felis venenatis blandit sed est
+      ultrices, adipiscing urna.
+    </div>
+    <div class="comments__form">
+      <input
+        type="text"
+        name="name"
+        class="comments__input"
+        placeholder="Name"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        class="comments__input"
+        placeholder="Email Address"
+        required
+      />
+      <textarea
+        name="comment"
+        class="comments__textarea"
+        placeholder="Comment"
+        required
+      ></textarea>
+    </div>
+    <button class="comments__button" type="submit">Submit</button>
   </div>
 </template>
 
